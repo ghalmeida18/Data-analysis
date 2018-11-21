b7338ae1783299cb3ee5ebe802fbb092e042d01d
ghalmeida18 <37963302+ghalmeida18@users.noreply.github.com>
2018-11-21 13:43:32 -0200

    Delete analise.r

--- analise.r
+++
@@ -1,9 +0,0 @@
-
-data <- read.csv("Salaries.csv")
-pagMax <- max(data$TotalPay)
-print (paste("Valor maximo pago: $",pagMax))
-pagMin <- min(data$TotalPay)
-print (paste("Valor minimo pago: $",pagMin))
-pagMed <- mean(data$TotalPay)
-print (paste("Media de Pagamentos: $",pagMed))
-print(paste("Cargo mais bem pago :",(subset(data, TotalPay == pagMax))$JobTitle))
